a19b13ba336816e8591200de3427e9e0fea8d34c
Luis Mariscal <luisferdev16@gmail.com>
2024-03-19 17:25:36 -0600

    Template strings

---
+++ js/08.js
@@ -0,0 +1,10 @@
+// Template Strings y Contatenación
+
+const producto = "Audifonos In-Ear";
+const precio = 4500;
+const marca = "Huawei"
+
+console.log('El producto es: ' + producto);
+console.log(`El producto es: ${producto}`);
+
+console.log(`${producto} $${precio} MXN, Marca: ${marca}`)
