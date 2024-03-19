a175354943dda29e5fbdbba8c482d1b0da6662bd
Luis Mariscal <luisferdev16@gmail.com>
2024-03-18 20:55:00 -0600

    objects

---
+++ js/04.js
@@ -0,0 +1,34 @@
+// Objetos
+const producto = {
+    nombre : 'Tablet',
+    precio: 300,
+    disponible: true
+}
+
+// console.log(producto)
+// console.table(producto)
+// console.log(producto.nombre)
+
+// Destructuring
+// const {nombre, precio, disponible} = producto;
+// console.log(nombre)
+// console.log(precio)
+// console.log(disponible)
+
+// const nombre = producto.nombre
+// const precio = producto.precio
+// const disponible = producto.disponible
+// console.log(precio)
+// console.log(nombre)
+// console.log(disponible)
+
+// Object Literal Enhacement 
+const autenticado = true
+const usuario = 'Luis'
+
+const nuevoObj = {
+    autenticado,
+    usuario
+}
+
+console.log(nuevoObj)
