e48c7123dd0d5542591f3f5d5225dc46e87b92f2
Luis Mariscal <luisferdev16@gmail.com>
2024-03-19 21:52:39 -0600

    arrays

---
+++ js/11.js
@@ -0,0 +1,8 @@
+const stack = ["Java", "NodeJS", "HTML", "CSS", "ReactJS"];
+
+const react = stack[4]
+
+//  Destructuring arrays
+const [ , , , , reactjs] = stack
+
+console.log(reactjs)
---
+++ js/12.js
@@ -0,0 +1,5 @@
+const stack = ["Java", "NodeJS", "HTML", "CSS", "ReactJS", "TypeScript"];
+
+for(let i = 0; i < stack.length; i++) {
+    console.log(stack[i])
+}
