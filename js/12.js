f10f4030c000f6095c6d9a9e6cd0a710f96ddf79
Luis Mariscal <luisferdev16@gmail.com>
2024-03-19 22:29:15 -0600

    arrays

--- js/12.js
+++ js/12.js
@@ -1,5 +1,20 @@
 const stack = ["Java", "NodeJS", "HTML", "CSS", "ReactJS", "TypeScript"];
 
-for(let i = 0; i < stack.length; i++) {
-    console.log(stack[i])
+// for(let i = 0; i < stack.length; i++) {
+//     console.log(stack[i])
+// }
+
+// ForEach
+// stack.forEach(function(tech) {
+//     console.log(tech)
+// })
+
+// Map
+const arrayMap = stack.map(function(tech) {
+    return tech
+})
+
+// for ... of
+for(let tech of stack) {
+    console.log(tech)
 }
