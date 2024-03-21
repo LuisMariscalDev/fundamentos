d42bd0757ae546581925d4bd468bfe5b3bdc765b
Luis Mariscal <luisferdev16@gmail.com>
2024-03-21 17:09:13 -0600

    Fetch API

---
+++ js/25.js
@@ -0,0 +1,13 @@
+// Fetch API con Promises
+
+const url = 'https://jsonplaceholder.typicode.com/comments'
+
+fetch(url)
+    .then((response) =>{
+        if(response.ok) {
+            return response.json()
+        }
+        throw new Error('Ocurrió un error')
+    })
+    .then(data =>  console.log(data))
+    .catch(error => console.log(error.message))
---
+++ js/26.js
@@ -0,0 +1,28 @@
+// Fetch API - Async Await
+
+const url = 'https://jsonplaceholder.typicode.com/comments'
+
+// fetch(url)
+//     .then((response) =>{
+//         if(response.ok) {
+//             return response.json()
+//         }
+//         throw new Error('Ocurrió un error')
+//     })
+//     .then(data =>  console.log(data))
+//     .catch(error => console.log(error.message))
+
+const consultarAPI = async () => {
+    try {
+        const response = await fetch(url)
+        if(!response.ok){
+            throw new Error('Ocurrió un error')
+        }
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+consultarAPI()
