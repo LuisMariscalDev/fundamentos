c3669cf13316a00a759b37eb619e63c6ad042895
Luis Mariscal <luisferdev16@gmail.com>
2024-03-20 20:08:48 -0600

    condicionales y comparadores

---
+++ js/18.js
@@ -0,0 +1,10 @@
+// Condicionales
+const disponible = 400;
+const retirar = 2000;
+
+if(disponible >= retirar) {
+    // Sí se cumple la condición
+    console.log('Sí puedes retirar');
+} else {
+    console.log('Saldo insuficiente')
+}
---
+++ js/19.js
@@ -0,0 +1,13 @@
+const var1 = 20
+const var2 = "20"
+
+/*
+    == (Comparador no estricto)
+    === (Comparador estricto)
+*/
+
+if(var1 === var2) {
+    console.log('Sí, son iguales');
+} else{
+    console.log('No, no son iguales');
+}
